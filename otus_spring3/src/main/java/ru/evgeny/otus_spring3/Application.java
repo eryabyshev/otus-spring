4bab1021f08d5a910ab050b50d8a17a5a8f46ad0
Evgeny Ryabyshew <ru.evgeny.dev@gmail.com>
2019-03-02 17:34:02 +0300

    complete hw5 - commited all nessosary files

---
+++ otus_spring3/src/main/java/ru/evgeny/otus_spring3/Application.java
@@ -0,0 +1,11 @@
+package ru.evgeny.otus_spring3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ otus_spring3/src/test/java/ru/evgeny/otus_spring3/OtusSpring3ApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.evgeny.otus_spring3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OtusSpring3ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
